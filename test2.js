affba1c6f2269a72f662dcffc69ea3ca7d4144d0
Ghaithq <gaoia123@gmail.com>
2024-10-16 15:51:43 +0300

    s

---
+++ test2.js
@@ -0,0 +1,35 @@
+const handleRefferal = async (
+    backendUrl,
+    iosRedirectUrl,
+    androidRedirectUrl
+) => {
+    const params = new URLSearchParams(window.location.search);
+    const inviterId = params.get("inviterId");
+
+    if (inviterId) {
+        // Collect device info
+        const screenWidth = window.screen.width;
+        const screenHeight = window.screen.height;
+        await fetch(backendUrl+`/store-referral`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                userAgent: window.navigator.userAgent,
+                screenWidth: screenWidth,
+                screenHeight: screenHeight,
+                inviterId: inviterId,
+            }),
+        });
+    }
+    if (/iPhone|iPad|iPod/i.test(userAgent)) {
+      window.location.href = iosRedirectUrl;
+    } else if (/Android/i.test(userAgent)) {
+      window.location.href = androidRedirectUrl;
+    } else {
+      console.error("Unsupported OS");
+    }
+};
+
+handleRefferal("http://localhost:5500","","");
